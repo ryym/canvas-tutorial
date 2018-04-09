c47df2ec3bf2b4b8dafb469b9c85d66037699226
ryym <ryym.64@gmail.com>
2018-04-09 17:13:21 +0900

    Enable to add canvas dynamically

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
+const canvasContainer = document.querySelector('.canvas-container');
+if (!canvasContainer) {
+  throw new Error('canvas container not found');
+}
+
+function makeAndDraw(id, drawer) {
+  const canvas = document.createElement('canvas');
+  canvas.id = id;
+  canvas.width = canvas.height = 150;
+  canvasContainer.appendChild(canvas);
+  drawer(canvas, canvas.getContext('2d'));
+}
+
 function draw() {
-  const canvas = document.getElementById('tutorial');
-  if (!canvas.getContext) {
+  if (!document.createElement('canvas').getContext) {
     console.log('Canvas not supported');
     return;
   }
-  const ctx = canvas.getContext('2d');
 
-  ctx.fillStyle = 'rgb(200, 0, 0)';
-  ctx.fillRect(10, 10, 50, 50);
-  ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-  ctx.fillRect(30, 30, 50, 50);
+  makeAndDraw('intersecting-rectangles', (canvas, ctx) => {
+    ctx.fillStyle = 'rgb(200, 0, 0)';
+    ctx.fillRect(10, 10, 50, 50);
+    ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+    ctx.fillRect(30, 30, 50, 50);
+  });
 }
 
 draw();
