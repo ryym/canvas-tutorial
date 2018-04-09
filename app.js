e06b2f77b31853d6036e0319ecc1a4fd6a11d7b1
ryym <ryym.64@gmail.com>
2018-04-09 16:43:47 +0900

    Start drawing

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('hello, world');
+function draw() {
+  const canvas = document.getElementById('tutorial');
+  if (!canvas.getContext) {
+    console.log('Canvas not supported');
+    return;
+  }
+  const ctx = canvas.getContext('2d');
+  console.log(ctx);
+}
+
+draw();
