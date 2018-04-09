74b70258d9f32a3a241dd70db06926cd0812de4c
ryym <ryym.64@gmail.com>
2018-04-09 17:28:45 +0900

    Draw triangles and arcs using paths

--- app.js
+++ app.js
@@ -29,6 +29,43 @@ function draw() {
     ctx.clearRect(45, 45, 60, 60);
     ctx.strokeRect(50, 50, 50, 50);
   });
+
+  makeAndDraw('draw-triangles-using-paths', (canvas, ctx) => {
+    ctx.beginPath();
+    ctx.moveTo(75, 50);
+    ctx.lineTo(100, 75);
+    ctx.lineTo(100, 25);
+    ctx.fill();
+
+    ctx.moveTo(25, 50);
+    ctx.lineTo(50, 75);
+    ctx.lineTo(50, 25);
+    ctx.closePath(); // ctx.lineTo(25, 50);
+    ctx.stroke();
+  });
+
+  const PI = Math.PI;
+
+  // 円の描画は右端の点からスタートするっぽい。
+  // 最後の boolean で時計回りかどうかを指定できる。
+  // trueなら反時計周り (anticlockwise)。
+  makeAndDraw('circles', (canvas, ctx) => {
+    ctx.beginPath();
+    ctx.arc(75, 75, 70, 0, PI * 2); // A circle
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.arc(75, 75, 60, 0, PI * 1); // 半円
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.arc(75, 75, 50, 0, PI * 1.5); // 右上以外
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.arc(75, 75, 40, 0, PI * 1.5, true); // 右上のみ
+    ctx.stroke();
+  });
 }
 
 draw();
