4fa5d825e4768edd7bfe4eeb485e616d99f4c00f
ryym <ryym.64@gmail.com>
2018-04-09 16:46:07 +0900

    Draw intersecting rectangles

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ function draw() {
     return;
   }
   const ctx = canvas.getContext('2d');
-  console.log(ctx);
+
+  ctx.fillStyle = 'rgb(200, 0, 0)';
+  ctx.fillRect(10, 10, 50, 50);
+  ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+  ctx.fillRect(30, 30, 50, 50);
 }
 
 draw();
