f9feb14c7634fc541126e52e6d8c5c4e4fb2a293
ryym <ryym.64@gmail.com>
2018-04-10 21:19:42 +0900

    Draw color circles

--- 03.js
+++ 03.js
@@ -9,6 +9,8 @@ function draw() {
 
   assertCanvasIsSupported();
 
+  const {PI} = Math;
+
   makeAndDraw('color-rectangles', ctx => {
     for (let a = 0; a < 6; a++) {
       const red = Math.floor(255 - 42.5 * a);
@@ -16,6 +18,19 @@ function draw() {
         const green = Math.floor(255 - 42.5 * b);
         ctx.fillStyle = `rgb(${red}, ${green}, 0)`;
         ctx.fillRect(b * 25, a * 25, 25, 25);
+      }
+    }
+  });
+
+  makeAndDraw('color-circles', ctx => {
+    for (let a = 0; a < 6; a++) {
+      const blue = Math.floor(255 - 42.5 * a);
+      for (let b = 0; b < 6; b++) {
+        const green = Math.floor(255 - 42.5 * b);
+        ctx.strokeStyle = `rgb(0, ${green}, ${blue})`;
+        ctx.beginPath();
+        ctx.arc(12.5 + a * 25, 12.5 + b * 25, 10, 0, PI * 2);
+        ctx.stroke();
       }
     }
   });
