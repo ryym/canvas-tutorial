e244bc64b7430d396289d490430dd5c04d8fa5e1
ryym <ryym.64@gmail.com>
2018-04-10 21:12:02 +0900

    Draw color rectangles

--- 03.js
+++ 03.js
@@ -8,6 +8,17 @@ function draw() {
   } = new Util().generate();
 
   assertCanvasIsSupported();
+
+  makeAndDraw('color-rectangles', ctx => {
+    for (let a = 0; a < 6; a++) {
+      const red = Math.floor(255 - 42.5 * a);
+      for (let b = 0; b < 6; b++) {
+        const green = Math.floor(255 - 42.5 * b);
+        ctx.fillStyle = `rgb(${red}, ${green}, 0)`;
+        ctx.fillRect(b * 25, a * 25, 25, 25);
+      }
+    }
+  });
 }
 
 draw();
